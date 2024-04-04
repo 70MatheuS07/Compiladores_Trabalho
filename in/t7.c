e9e17285418aa3f43705aae36c9b559d923cdc2a
Matheus <matheusdeoliveiraes@gmail.com>
2024-04-04 15:47:09 -0300

    direto na main, CP1 finalizado!

---
+++ in/t7.c
@@ -0,0 +1,22 @@
+int RetornaDiferenca(int x, int y){
+    if(x>y) return 1;
+    else if(x<y) return -1;
+    return 0;
+}
+
+int main ()
+{
+    int x = 2;
+    int y = 2;
+    
+    if(RetornaDiferenca(x,y) == 0)
+    {
+        printf("X igual Y");
+    }
+    else
+    {
+        printf("X diferente Y");
+    }
+
+    return 0;
+}
---
+++ in/t8.c
@@ -0,0 +1,11 @@
+int main ()
+{
+    int vet[2];
+
+    vet[0] = 10;
+    vet[1] = 20;
+
+    printf("%d > %d\n", vet[1], vet[0]);
+
+    return 0;
+}
